9697a8795ef0afbe62aa0942d974e2ef23a27f44
gord0n0 <itsgordon4@gmail.com>
2025-02-19 12:38:22 +0300

    first commit

---
+++ src/components/Homepage.vue
@@ -0,0 +1,113 @@
+<template>
+    <v-card class="mx-auto" elevation="24">
+
+<v-carousel hide-delimiters>
+    <v-carousel-item src= "../public/image 1.jpg" cover> </v-carousel-item>
+    <v-carousel-item src="../public/image 2.jpg" cover> </v-carousel-item>
+    <v-carousel-item src="../public/image 3.jpg" cover> </v-carousel-item>
+    <v-carousel-item src="../public/image 5.jpg" cover> </v-carousel-item>
+</v-carousel>
+
+</v-card>
+<v-container>
+        <v-card class="mx-auto" elevation="24">
+            <v-container>
+            <v-row>
+                <v-col cols="12" md="4">
+                    <v-card>
+                        <v-card color="black">
+                            <v-card-item>
+                                <v-card-title>Promotion</v-card-title>
+
+                                <v-card-subtitle>Limited Offer!!!</v-card-subtitle>
+                            </v-card-item>
+                            <v-img height="200px" src="../public/food 1.jpg" cover ></v-img>
+
+                            <v-card-text>
+                           Hot Spicy Chicken Wings =799 KES
+                            </v-card-text>
+                        </v-card>
+                        <v-card color="black">
+                            <v-card-item>
+                            <v-card-title>Promotions</v-card-title>
+
+                            <v-card-subtitle>Limited Offer!!!</v-card-subtitle>
+                            </v-card-item>
+                            <v-img height="200px" src="../public/drink 3.avif"cover ></v-img>
+
+                            <v-card-text>
+                            Dragon fruit drinks = 599 KES
+                            </v-card-text>
+                        </v-card>
+                    </v-card>
+
+                </v-col>
+
+                <v-col cols="12" md="4">
+                    <v-card>
+                    <v-card color="black">
+                    <v-card-item>
+                    <v-card-title>Upcoming Events</v-card-title>
+
+                    <v-card-subtitle>Eat till you drop</v-card-subtitle>
+                    </v-card-item>
+                    <v-img height="200px" src="../public/food 4.jpg" cover ></v-img>
+
+                    <v-card-text>
+                   All you can eat for the price =4999 KES per person
+                    </v-card-text>
+                    </v-card>
+                    <v-card color="black">
+                    <v-card-item>
+                    <v-card-title>Upcomming Events</v-card-title>
+
+                    <v-card-subtitle>Drink Tasting</v-card-subtitle>
+                    </v-card-item>
+                    <v-img height="200px" src="../public/drink 4.avif" cover ></v-img>
+
+                    <v-card-text>
+                    31st March dont miss out =1000 KES Per Person
+                    </v-card-text>
+                    </v-card>
+                </v-card>
+
+                </v-col>
+
+                <v-col cols="12" md="4">
+                    <v-card>
+                        <v-card color="black">
+                    <v-card-item>
+                    <v-card-title>Promotions</v-card-title>
+
+                    <v-card-subtitle>This is a card subtitle</v-card-subtitle>
+                    </v-card-item>
+                    <v-img height="200px" src="../public/food 3.jpg" cover ></v-img>
+
+                    <v-card-text>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!
+                    </v-card-text>
+                        </v-card>
+                        <v-card color="yellow">
+                    <v-card-item>
+                    <v-card-title>Promotions</v-card-title>
+
+                    <v-card-subtitle>This is a card subtitle</v-card-subtitle>
+                    </v-card-item>
+                    <v-img height="200px" src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" cover ></v-img>
+
+                    <v-card-text>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!
+                    </v-card-text>
+                        </v-card>
+                    </v-card>
+
+                </v-col>
+            </v-row>
+        </v-container>
+        </v-card>
+    </v-container>
+</template>
+
+<script setup>
+
+</script>
---
+++ src/components/Restaurant.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>Restaurant List</h1>
+</template>
+
+<script setup>
+
+</script>
